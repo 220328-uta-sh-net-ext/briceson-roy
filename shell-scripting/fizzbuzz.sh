3a5a52184ca6e330e91919d188b3cccf593ca51d
Briceson <royzone4@gmail.com>
2022-03-30 16:36:04 -0400

    let's play fizzbuzz!

---
+++ shell-scripting/fizzbuzz.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/bash 
+
+read -p "Enter A Number!" number
+
+if test $number -gt 0 && test $number -lt 21
+then
+    if test $((number % 5)) -eq 0 && test $((number % 3)) -eq 0;
+        then
+            echo "fizzbuzz"
+    elif test $((number % 5)) -eq 0
+        then
+            echo "fizz"
+    elif test $((number % 3)) -eq 0
+        then   
+            echo "buzz"
+    else
+        echo "boom"
+    fi
+fi
